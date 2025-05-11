ef740e70d38da4e65ae193baf0ec24ed6507fd56
dimasrhesky270206 <dimashandaresky@gmail.com>
2025-05-11 18:12:45 +0700

    done ga bang

--- JOBSHEET-9/MahasiswaDemo06.java
+++ JOBSHEET-9/MahasiswaDemo06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
- public class MahasiswaDemo06 {
+public class MahasiswaDemo06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int pilih;
